566132127bfa010e2ee76616127b2d4e30417335
Kirill <balanovichks@gmail.com>
2021-03-02 17:23:48 +0300

    create mongo db connection and seed some data to product collection

---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "This is iphone",
+                    Description = "Lorem ipsum dolor",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart phone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S8",
+                    Summary = "This is samsung",
+                    Description = "Lorem ipsum dolor",
+                    ImageFile = "product-2.png",
+                    Price = 950.00M,
+                    Category = "Smart phone"
+                },
+                new Product()
+                {
+                    Name = "Asus rog phone",
+                    Summary = "This is asus",
+                    Description = "Lorem ipsum dolor",
+                    ImageFile = "product-3.png",
+                    Price = 990.00M,
+                    Category = "Smart phone"
+                },
+                new Product()
+                {
+                    Name = "Xiaomi",
+                    Summary = "This is xiaomi",
+                    Description = "Lorem ipsum dolor",
+                    ImageFile = "product-4.png",
+                    Price = 600.00M,
+                    Category = "Smart phone"
+                }
+            };
+        }
+    }
+}
--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
@@ -31,6 +35,11 @@ namespace Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Catalog.API", Version = "v1"});
             });
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+            services.AddSingleton<ICatalogDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
+
+            services.AddTransient<ICatalogContext, CatalogContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
