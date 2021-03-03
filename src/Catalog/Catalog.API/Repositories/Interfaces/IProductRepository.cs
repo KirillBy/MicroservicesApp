422d163b32500a5e23e42954471505745462a839
Kirill <balanovichks@gmail.com>
2021-03-03 11:54:10 +0300

    Catalog microservice done

---
+++ src/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProducts();
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+    }
+}
